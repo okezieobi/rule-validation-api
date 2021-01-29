443432cbd8e8c9a2a58b8dd8c06a4089c24fbab9
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2021-01-29 22:07:08 +0100

    Feature debugged

--- src/services.js
+++ src/services.js
@@ -1,32 +1,45 @@
 /* eslint-disable camelcase */
-const validateRule = (payload) => {
+const validateRule = ({ rule, data }) => {
   let result;
   const preValidation = {
     status: 'error',
     data: null,
     statusCode: 400,
   };
-  if (!payload || typeof payload !== 'object') {
-    preValidation.message = 'Invalid JSON payload.';
-    result = { ...preValidation };
-  } else if (!payload.rule) {
+  if (!rule) {
     preValidation.message = 'rule is required.';
     result = { ...preValidation };
-  } else if (typeof payload.rule !== 'object') {
+  } else if (typeof rule !== 'object') {
     preValidation.message = 'rule must be an object.';
     result = { ...preValidation };
-  } else if (payload.data) {
+  } else if (!data) {
     preValidation.message = 'data is required.';
+    result = { ...preValidation };
+  } else if (typeof data !== 'object') {
+    preValidation.message = 'Invalid JSON payload.';
     result = { ...preValidation };
   } else {
     const conditionTest = ['eq', 'neq', 'gt', 'gte'];
-    const { field, condition, condition_value } = payload.rule;
-    if (!field) result = { message: 'rule field is required.' };
-    else if (!condition) result = { message: 'rule condition is required.' };
-    else if (!conditionTest.includes(condition)) result = { message: 'rule condition is invalid.' };
-    else if (!condition_value) result = { message: 'rule condition value is required.' };
-    else if (!Object.keys(payload.data).includes(field)) result = { message: `field ${field} is missing from data` };
-    else {
+    const { field, condition, condition_value } = rule;
+    if (!field) {
+      preValidation.message = 'rule field is required.';
+      result = { ...preValidation };
+    } else if (!condition) {
+      preValidation.message = 'rule condition is required.';
+      result = { ...preValidation };
+    } else if (!conditionTest.includes(condition)) {
+      preValidation.message = 'rule condition is invalid.';
+      result = { ...preValidation };
+    } else if (!condition_value) {
+      preValidation.message = 'rule condition value is required.';
+      result = { ...preValidation };
+    } else if (!data[field]) {
+      preValidation.message = `field ${field} is missing from data.`;
+      result = { ...preValidation };
+    } else if (typeof data[field] !== typeof condition_value) {
+      preValidation.message = `field ${field} should be a|an ${typeof condition_value}.`;
+      result = { ...preValidation };
+    } else {
       const validationPassed = {
         message: `field ${field} successfully validated.`,
         status: 'success',
@@ -47,22 +60,22 @@ const validateRule = (payload) => {
         },
       };
       if (condition === 'eq') {
-        if (payload.data[field] === condition_value) result = { ...validationPassed };
+        if (data[field] === condition_value) result = { ...validationPassed };
         else result = { ...validationFailed };
       } else if (condition === 'neq') {
-        if (payload.data[field] === condition_value) result = { ...validationFailed };
+        if (data[field] === condition_value) result = { ...validationFailed };
         else result = { ...validationPassed };
       } else if (condition === 'gt') {
-        if (payload.data[field] > condition_value && typeof payload.data[field] === 'number' && typeof condition_value === 'number') result = { ...validationPassed };
+        if (data[field] > condition_value && typeof data[field] === 'number' && typeof condition_value === 'number') result = { ...validationPassed };
         else result = { ...validationFailed };
       } else if (condition === 'gte') {
-        if (payload.data[field] >= condition_value && typeof payload.data[field] === 'number' && typeof condition_value === 'number') result = { ...validationPassed };
+        if (data[field] >= condition_value && typeof data[field] === 'number' && typeof condition_value === 'number') result = { ...validationPassed };
         else result = { ...validationFailed };
       }
       result.data.validation.condition_value = condition_value;
       result.data.validation.condition = condition;
       result.data.validation.field = field;
-      result.data.validation.field_value = payload.data[field];
+      result.data.validation.field_value = data[field];
     }
   }
   return result;
